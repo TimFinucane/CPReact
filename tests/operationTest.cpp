0841dadaa719e5a9a9ef88c7465db8b57ac7fa1d
Timothy Finucane <timfinucane@outlook.com>
2018-03-05 16:22:14 +1300

    Test for using external functions

--- tests/operationTest.cpp
+++ tests/operationTest.cpp
@@ -1,23 +1,38 @@
 #include "CppUnitTest.h"
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
+#include <cmath>
+
 #include "Operation.h"
-#include "Reactive.h"
+#include "Constant.h"
 using namespace react;
 
-TEST_CLASS(OperationTest)
+TEST_CLASS( OperationTest )
 {
-    TEST_METHOD(simpleHypotenuse)
+    TEST_METHOD( simpleHypotenuse )
     {
-        Reactive<int> opposite = 3;
-        Reactive<int> adjacent = 5;
+        Constant<int> opposite = 3;
+        Constant<int> adjacent = 5;
 
         auto hypotenuseSquared = (opposite * opposite) + (adjacent * adjacent);
 
-        Assert::AreEqual(34, hypotenuseSquared.get());
+        Assert::AreEqual( 34, hypotenuseSquared.get() );
 
         opposite = 5;
 
-        Assert::AreEqual(50, (int)hypotenuseSquared);
+        Assert::AreEqual( 50, (int)hypotenuseSquared );
+    }
+
+    TEST_METHOD( simpleExternalFunction )
+    {
+        Constant<float> angle = 3.14159f;
+
+        auto ratio = make_operation( std::sinf, angle );
+
+        Assert::AreEqual( 0.0f, ratio(), 0.1f );
+
+        angle = 3.14159f / 2.0f;
+
+        Assert::AreEqual( 1.0f, ratio(), 0.1f );
     }
 };
